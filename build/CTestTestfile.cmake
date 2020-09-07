267f6d53ed543dee1625c6cc461ec1a80f9f6f40
Am <am@home.lan>
2020-09-07 14:05:23 -0700

    Initialize Ros Project

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/am/ros_project/src
+# Build directory: /home/am/ros_project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
