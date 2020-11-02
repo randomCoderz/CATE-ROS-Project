49de404cc9f598822f128ed3b557534508fd65e8
Am <am@home.lan>
2020-11-01 17:07:07 -0800

    Updates

--- src/code_test_python/src/talker.py
+++ src/code_test_python/src/talker.py
@@ -40,7 +40,7 @@ import rospy
 from std_msgs.msg import String
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+    pub = rospy.Publisher('/husky_velocity_controller/cmd_vel geometry_msgs/Twist', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
